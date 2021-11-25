0488b945f133fbdd3e9a1f1e372132b97fda2cda
gribdesbois <gribdesbois@gmail.com>
2021-11-25 22:45:04 +0100

    toying with console.log

--- src/components/Home/Services/Marketing/Marketing.js
+++ src/components/Home/Services/Marketing/Marketing.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Marketing() {
+  const location = useLocation()
+  const values = Object.values(location)
+  console.log(location)
   return (
     <div>
-      <h1>Nos services de Marketing</h1>
+      <h1>Nos services de Marketing {values} </h1>
     </div>
   )
 }
